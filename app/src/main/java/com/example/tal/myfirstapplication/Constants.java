413b6018b52e9cc6e8bf78b11431ef9f60999aa1
gronteza <gronteza@gmail.com>
2016-03-23 23:25:54 +0200

    MainGame 1st gen

--- app/src/main/java/com/example/tal/myfirstapplication/Constants.java
+++ app/src/main/java/com/example/tal/myfirstapplication/Constants.java
@@ -28,7 +28,7 @@ public class Constants extends Activity {
     public static final String MY_MESSAGE = "my message";
     public static final String ID_EXTRA = "extra";
     //public static final String REST_API_PREFIX = "http://192.168.1.11:8080/DixitRESTfulAPI/rest/service/";
-    public static final String REST_API_PREFIX = "http://192.168.14.110:8080/DixitRESTfulAPI/rest/service/";
+    public static final String REST_API_PREFIX = "http://192.168.14.197:8080/DixitRESTfulAPI/rest/service/";
     public static final String ADD_ROOM_API_URL = REST_API_PREFIX + "addRoom";
     public static final String ADD_PLAYER_TO_ROOM_API_URL = REST_API_PREFIX + "join";
     public static final String GET_ROOMS_API_URL = REST_API_PREFIX + "rooms";
--- app/src/main/java/com/example/tal/myfirstapplication/GameMain.java
+++ app/src/main/java/com/example/tal/myfirstapplication/GameMain.java
@@ -1,5 +1,8 @@
 package com.example.tal.myfirstapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.DragEvent;
 import android.view.View;
+import android.view.animation.AnimationSet;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -35,13 +39,20 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
     Point po;
     public static Vibrator vib;
     Context context;
+    boolean usr1 = false;
+    boolean usr2 = false;
+    boolean usr3 = false;
+    AnimatorSet anset1;
+    AnimatorSet anset2;
+    AnimatorSet anset3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_main);
         draggedView = new Card();
-        cardSize=0;
+        cardSize = 0;
         po = new Point();
         data = ClipData.newPlainText("", "");
         vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
@@ -56,18 +67,36 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
     }
 
 
-
-
     @Override
     public void onClick(View v) {
-        if (v.getClass() == ImageView.class) {
-            int i =  getListPlaceByView(v);
-            if (v.getLayoutParams().height < cardSize * 5 && i >=0) {
+        if (getListPlaceByView(v) != -1) {
+            int i = getListPlaceByView(v);
+            if (v.getLayoutParams().height < cardSize * 5 && i >= 0) {
                 setBigCardLayout(getListPlaceByView(v));
             } else {
-                setAllCards(cardSize, cardSize*2);
+                setAllCards(cardSize, cardSize * 2);
             }
-        }
+        } /*else if (v.getId() == R.id.user1) {
+            if (usr1) {
+
+                usr1 = false;
+            } else {
+                usr1 = true;
+
+            }
+        } else if (v.getId() == R.id.user2) {
+            if (usr2) {
+                usr2 = false;
+            } else {
+                usr2 = true;
+            }
+        } else if (v.getId() == R.id.user3) {
+            if (usr3) {
+                usr3 = false;
+            } else {
+                usr3 = true;
+            }
+        }*/
     }
 
     @Override
@@ -76,11 +105,11 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
             draggedView = cardsInHand.get(getListPlaceByView(v));
             View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
             v.startDrag(data, shadowBuilder, v, 0);
-            v.setVisibility(View.INVISIBLE);
+            draggedView.setVisibility(View.INVISIBLE);
             GameMain.vib.vibrate(20);
             targetCard.setVisibility(View.VISIBLE);
             targetCard.bringToFront();
-            isCardOnTable=true;
+            isCardOnTable = true;
             return true;
         }
         return false;
@@ -95,12 +124,12 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
                 break;
             case DragEvent.ACTION_DRAG_EXITED:
                 draggedView.cardPic.setVisibility(View.VISIBLE);
-                setAllCards(cardSize,cardSize*2);
+                setAllCards(cardSize, cardSize * 2);
                 break;
             case DragEvent.ACTION_DROP:
-                if (findViewById(R.id.table) == v){
+                if (findViewById(R.id.table) == v) {
                     int i = getListPlaceByView(draggedView.cardPic);
-                    if (i >= 0){
+                    if (i >= 0) {
                         cardsInHand.remove(i);
                         draggedView.cardPic.setLayoutParams(getOutgoingCardLayoutParams());
                         rearrangeCards();
@@ -119,33 +148,28 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
     }
 
     public void setCardsInPosition() {
-
         ImageView plr1 = (ImageView) findViewById(R.id.user1);
         ImageView plr2 = (ImageView) findViewById(R.id.user2);
         ImageView plr3 = (ImageView) findViewById(R.id.user3);
-
-        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(100,100);
-        lp1.leftMargin = plr1.getLayoutParams().width/2;
-        lp1.topMargin = po.y / 5;
-        plr1.setLayoutParams(lp1);
-
-        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(100,100);
-        lp2.rightMargin = plr2.getLayoutParams().width/2;
-        lp2.topMargin = po.y / 5;
-        plr2.setLayoutParams(lp2);
-
-        RelativeLayout.LayoutParams lp3 = new RelativeLayout.LayoutParams(100,100);
-        lp3.leftMargin = po.x/2;
-        lp3.topMargin = plr3.getLayoutParams().width/2;
-        plr3.setLayoutParams(lp3);
+/*
+        plr1.setOnClickListener(this);
+        plr2.setOnClickListener(this);
+        plr3.setOnClickListener(this);
+*/
 
 
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card1),this, (TextView) findViewById(R.id.card1text)));
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card2),this, (TextView) findViewById(R.id.card2text)));
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card3),this, (TextView) findViewById(R.id.card3text)));
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card4),this, (TextView) findViewById(R.id.card4text)));
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card5),this, (TextView) findViewById(R.id.card5text)));
-        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card6),this, (TextView) findViewById(R.id.card6text)));
+        anset1 = (AnimatorSet)AnimatorInflater.loadAnimator(this,R.animator.user1movement);
+        anset2 = (AnimatorSet)AnimatorInflater.loadAnimator(this,R.animator.user2movement);
+        anset3 = (AnimatorSet)AnimatorInflater.loadAnimator(this,R.animator.user3movement);
+
+        anset1.setTarget(plr1);
+
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card1), this, (TextView) findViewById(R.id.card1text)));
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card2), this, (TextView) findViewById(R.id.card2text)));
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card3), this, (TextView) findViewById(R.id.card3text)));
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card4), this, (TextView) findViewById(R.id.card4text)));
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card5), this, (TextView) findViewById(R.id.card5text)));
+        cardsInHand.add(new Card(1, 1, new RelativeLayout.LayoutParams(1, 1), (ImageView) findViewById(R.id.card6), this, (TextView) findViewById(R.id.card6text)));
         calcSize();
 
         targetCard = (ImageView) findViewById(R.id.target);
@@ -154,7 +178,7 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
         targetCard.bringToFront();
 
 
-        setAllCards(cardSize, cardSize*2);
+        setAllCards(cardSize, cardSize * 2);
         for (int i = 0; i < cardsInHand.size(); i++) {
             cardsInHand.get(i).cardPic.setOnClickListener(this);
             cardsInHand.get(i).cardPic.setOnLongClickListener(this);
@@ -175,8 +199,8 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
     }
 
     private int getListPlaceByView(View v) {
-        for (int i = 0; i < cardsInHand.size(); i++){
-            if (cardsInHand.get(i).cardPic == v){
+        for (int i = 0; i < cardsInHand.size(); i++) {
+            if (cardsInHand.get(i).cardPic == v) {
                 return i;
             }
         }
@@ -191,29 +215,28 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
         return labelLayoutParams;
     }
 
-    private void setBigCardLayout(int i){
-        int tmpCardSize = cardSize/2;
+    private void setBigCardLayout(int i) {
+        int tmpCardSize = cardSize / 2;
         int bigCardSize = cardSize * 5;
-        int tmpWideSize = (po.x - (bigCardSize + (cardsInHand.size()*tmpCardSize)))/(cardsInHand.size() +1);
+        int tmpWideSize = (po.x - (bigCardSize + (cardsInHand.size() * tmpCardSize))) / (cardsInHand.size() + 1);
         RelativeLayout.LayoutParams labelLayoutParams = null;
 
-        for (int j =0; j< cardsInHand.size();j++){
-            if (j<i){
-                labelLayoutParams = new RelativeLayout.LayoutParams(tmpCardSize, tmpCardSize*2);
+        for (int j = 0; j < cardsInHand.size(); j++) {
+            if (j < i) {
+                labelLayoutParams = new RelativeLayout.LayoutParams(tmpCardSize, tmpCardSize * 2);
                 labelLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
                 labelLayoutParams.bottomMargin = cardSize;
                 labelLayoutParams.leftMargin = (j * (tmpCardSize + tmpWideSize)) + tmpWideSize;
-            }
-            else if (j==i){
-                labelLayoutParams = new RelativeLayout.LayoutParams(bigCardSize, bigCardSize*2);
+            } else if (j == i) {
+                labelLayoutParams = new RelativeLayout.LayoutParams(bigCardSize, bigCardSize * 2);
                 labelLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
                 labelLayoutParams.bottomMargin = cardSize;
                 labelLayoutParams.leftMargin = (j * (tmpCardSize + tmpWideSize)) + tmpWideSize;
-            }else{
-                labelLayoutParams = new RelativeLayout.LayoutParams(tmpCardSize, tmpCardSize*2);
+            } else {
+                labelLayoutParams = new RelativeLayout.LayoutParams(tmpCardSize, tmpCardSize * 2);
                 labelLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
                 labelLayoutParams.bottomMargin = cardSize;
-                labelLayoutParams.leftMargin = ((j-1) * (tmpCardSize + tmpWideSize)) + (2*tmpWideSize)+bigCardSize;
+                labelLayoutParams.leftMargin = ((j - 1) * (tmpCardSize + tmpWideSize)) + (2 * tmpWideSize) + bigCardSize;
             }
             cardsInHand.get(j).cardPic.setLayoutParams(labelLayoutParams);
         }
@@ -221,9 +244,9 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
     }
 
     private RelativeLayout.LayoutParams getOutgoingCardLayoutParams() {
-        RelativeLayout.LayoutParams labelLayoutParams = new RelativeLayout.LayoutParams(cardSize, cardSize*2);
+        RelativeLayout.LayoutParams labelLayoutParams = new RelativeLayout.LayoutParams(cardSize, cardSize * 2);
         labelLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        labelLayoutParams.topMargin = 8*cardSize;
+        labelLayoutParams.topMargin = 8 * cardSize;
         return labelLayoutParams;
     }
 
@@ -232,25 +255,58 @@ public class GameMain extends Activity implements View.OnClickListener, View.OnL
         setAllCards(cardSize, cardSize * 2);
     }
 
+    public void moveUser1(View view){
+        if(usr1){
+            findViewById(R.id.username1).setVisibility(View.INVISIBLE);
+            usr1=false;
+        }else{
+            findViewById(R.id.username1).setVisibility(View.VISIBLE);
+            anset1.start();
+            usr1=true;
+        }
+    }
+    public void moveUser2(View view){
+        if(usr2){
+            findViewById(R.id.username2).setVisibility(View.INVISIBLE);
+
+            usr2=false;
+        }else{
+            findViewById(R.id.username2).setVisibility(View.VISIBLE);
+            usr2=true;
+        }
+    }
+    public void moveUser3(View view){
+        if(usr3){
+            findViewById(R.id.username3).setVisibility(View.INVISIBLE);
+
+            usr3=false;
+        }else{
+            findViewById(R.id.username3).setVisibility(View.VISIBLE);
+
+            usr3=true;
+        }
+    }
+
     private void calcSize() {
         this.getWindowManager().getDefaultDisplay().getSize(po);
 
         if (cardSize == 0) {
-            sizeW = po.x / (3*cardsInHand.size() + 1);
+            sizeW = po.x / (3 * cardsInHand.size() + 1);
             cardSize = 2 * sizeW;
-        }else{
-            sizeW = ((po.x -(cardsInHand.size()*cardSize)) / (cardsInHand.size() + 1));
+        } else {
+            sizeW = ((po.x - (cardsInHand.size() * cardSize)) / (cardsInHand.size() + 1));
         }
         sizeH = po.y;
     }
+
     private class OnClose extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
             JSONObject jobj = new JSONObject();
             try {
-                jobj.put(Constants.ROOM_FIELD,UserData.getInstance().getCurrRoom(context));
-                jobj.put(Constants.NAME_FIELD,UserData.getInstance().getNickName(context));
+                jobj.put(Constants.ROOM_FIELD, UserData.getInstance().getCurrRoom(context));
+                jobj.put(Constants.NAME_FIELD, UserData.getInstance().getNickName(context));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
