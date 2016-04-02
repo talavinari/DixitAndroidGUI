9f5b89bef83a92aa53bf5b7a787454423ace93a8
gronteza <gronteza@gmail.com>
2016-04-02 16:25:00 +0300

    dagfsd

--- app/src/main/java/com/example/tal/myfirstapplication/GameMain.java
+++ app/src/main/java/com/example/tal/myfirstapplication/GameMain.java
@@ -21,16 +21,12 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import org.json.JSONException;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
